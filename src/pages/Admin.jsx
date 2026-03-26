30abaf255214352b7c1d2f1730cda4920f626c0f
morozovsuno-sys <morozovsuno@gmail.com>
2026-03-26 13:06:49 +0500

    Add Admin page component

---
+++ src/pages/Admin.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+function Admin() {
+  return (
+    <div className="admin">
+      <div className="hero">
+        <h1>Администрирование</h1>
+        <p>Управление контентом</p>
+      </div>
+      <div className="container">
+        <p>Раздел в разработке...</p>
+      </div>
+    </div>
+  )
+}
+
+export default Admin
