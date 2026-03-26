ed1f21e433e115d7f3fcd49396d6a356924c2986
morozovsuno-sys <morozovsuno@gmail.com>
2026-03-26 13:06:09 +0500

    Add Blog and Admin page components

---
+++ src/pages/Blog.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+function Blog() {
+  return (
+    <div className="blog">
+      <div className="hero">
+        <h1>Блог</h1>
+        <p>Новости и обновления PromtWave</p>
+      </div>
+      <div className="container">
+        <p>Скоро здесь появятся статьи о создании музыки с Suno AI</p>
+      </div>
+    </div>
+  )
+}
+
+export default Blog
