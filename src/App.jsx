d26d046b4cfc4bbc97b8fcf4d3dd79d3e184df22
morozovsuno-sys <morozovsuno@gmail.com>
2026-03-26 13:02:03 +0500

    Add App component with routing structure

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import { useState } from 'react'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import './App.css'
+import Home from './pages/Home'
+import PromptGenerator from './pages/PromptGenerator'
+import Blog from './pages/Blog'
+import Admin from './pages/Admin'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <Router basename="/promtwave-v2">
+      <div className="App">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/generator" element={<PromptGenerator />} />
+          <Route path="/blog" element={<Blog />} />
+          <Route path="/admin" element={<Admin />} />
+        </Routes>
+      </div>
+    </Router>
+  )
+}
+
+export default App
