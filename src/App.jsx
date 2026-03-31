3085d4d65087db96d5539bca1d989347ae93bfc2
morozovsuno-sys <morozovsuno@gmail.com>
2026-03-31 16:05:36 +0500

    Update App.jsx with Navbar and router setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,16 @@
-import { useState } from 'react'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import './App.css'
+import Navbar from './components/Navbar'
 import Home from './pages/Home'
 import PromptGenerator from './pages/PromptGenerator'
 import Blog from './pages/Blog'
 import Admin from './pages/Admin'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <Router basename="/promtwave-v2">
       <div className="App">
+        <Navbar />
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/generator" element={<PromptGenerator />} />
