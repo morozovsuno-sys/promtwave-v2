a2f854fd29e707a1cadc481a8985f27809ca35e9
morozovsuno-sys <morozovsuno@gmail.com>
2026-03-31 16:20:24 +0500

    Add new routes for Structure, Mastering, and Examples

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,9 @@ import Home from './pages/Home'
 import PromptGenerator from './pages/PromptGenerator'
 import Blog from './pages/Blog'
 import Admin from './pages/Admin'
+import Structure from './pages/Structure'
+import Mastering from './pages/Mastering'
+import Examples from './pages/Examples'
 
 function App() {
   return (
@@ -16,6 +19,9 @@ function App() {
           <Route path="/generator" element={<PromptGenerator />} />
           <Route path="/blog" element={<Blog />} />
           <Route path="/admin" element={<Admin />} />
+                <Route path="/structure" element={<Structure />} />
+                <Route path="/mastering" element={<Mastering />} />
+                <Route path="/examples" element={<Examples />} />
         </Routes>
       </div>
     </Router>
